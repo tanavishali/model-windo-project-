e03bec2352cff6bf2beba8390b2a2babe0c8b685
tanavishali <tanawishalrai5271@gmail.com>
2024-07-17 14:21:55 +0500

    add close and open button js

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ let modal = document.querySelector(".modal");
 let overlay = document.querySelector(".overlay");
 let close_modal = document.querySelector(".close-modal");
 let show_modal = document.querySelectorAll(".show-modal");
-console.log(show_modal);
-for (let i = 0; i < show_modal.length; i++) {
-  console.log(
-    show_modal[i].addEventListener("click", function () {
-      console.log(document.querySelector(".show_model").textContent);
-    })
-  );
-}
+// console.log(show_modal, typeof show_modal, show_modal.length);
+let openmodel = function () {
+  console.log("button click");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+let close_modalfunc = function () {
+  modal.classList.add("hidden");
+
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < show_modal.length; i++)
+  show_modal[i].addEventListener("click", openmodel);
+
+close_modal.addEventListener("click", close_modalfunc);
+
+overlay.addEventListener("click", close_modalfunc);
